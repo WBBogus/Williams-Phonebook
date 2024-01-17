ced721d849e384a3dd4301e2f25281cf2a6df74b
ImSoBogus <willybogus11@gmail.com>
2024-01-17 14:34:42 -0600

    creating class & private variables
    adding private variables
    creating class

---
+++ Phone/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    public class Book
+    {
+        private string firstName = "n/a";
+        private string lastName = "n/a";
+        private string phoneNumber = "n/a";
+        private string address = "n/a";
+    }
+}
--- Phone/Program.cs
+++ Phone/Program.cs
@@ -8,7 +8,7 @@ namespace Phone
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
         }
     }
